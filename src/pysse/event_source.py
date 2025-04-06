c961eb6b1930ba81ea049436ac34d7caa351e26e
Dilanka Rathnasiri <rdilanka17@gmail.com>
2025-04-06 20:26:35 +0530

    update to use aiohttp

--- src/pysse/event_source.py
+++ src/pysse/event_source.py
@@ -1,32 +1,64 @@
 from event import Event
-import requests
 from typing import Callable
-from utils import get_string
 import json
+import aiohttp
+import asyncio
+from exceptions import HttpRespException
 
 
 class Client:
-    def __init__(self, url: str, callable: Callable):
-        self.url = url
-        self.callable = callable
-        self.closed = True
+    def __init__(self, url: str, callable: Callable, param={}, headers={}):
+        self.url: str = url
+        self.param: dict = param
+        self.headers: dict = headers
+        self.callable: Callable = callable
+        self.__task: asyncio.Task = None
+        self.__closed = True
 
-    def connect(self):
+    async def connect(self):
         """
-        Connect to Server Sent Events API
+        Asynchronously connect to Server Sent Events API
         """
         self.closed = False
-        stream = requests.get(self.url, stream=True)
+        async with aiohttp.ClientSession() as session:
+            async with session.get(
+                self.url, params=self.param, headers=self.headers
+            ) as response:
+                if response.status != 200:
+                    raise HttpRespException(
+                        message="Connection failed",
+                        status=response.status,
+                        headers=response.headers,
+                    )
+                self.__task = asyncio.create_task(self.__read_stream(response.content))
+                try:
+                    await self.__task
+                except asyncio.CancelledError:
+                    pass
+
+    async def __read_stream(self, stream: aiohttp.StreamReader) -> None:
+        """
+        Read from HTTP stream
+        """
         event: Event = Event("", "")
-        for line in stream.iter_lines():
-            if not line:  # new event
+        async for line in stream:
+            inp = line.decode("utf-8")
+            if inp == "\n":  # new event
                 self.callable(event)
                 continue
 
-            inp = get_string(line)
             if inp.startswith("event:"):
-                event.name = inp[6:]
+                event.name = inp[6:-1]
             elif inp.startswith("data:"):
                 event.data = json.loads(inp[5:])
-            if self.closed:
-                break
+
+    async def close(self):
+        """
+        Close the connection of server sent event stream
+        """
+        if not (self.__closed) and self.__task is not None:
+            self.__task.cancel()
+            self.__closed = True
+
+    def is_closed(self):
+        return self.__closed
--- src/pysse/utils.py
+++
@@ -1,5 +0,0 @@
-from typing import ByteString
-
-
-def get_string(input: ByteString):
-    return input.decode("utf-8")
